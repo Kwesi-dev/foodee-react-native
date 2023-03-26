97094f8107529b0aac00cad68030f04ed2b5c401
Afriyie Samuel Kwesi <afriyiesamuel36@gmail.com>
2023-03-26 05:46:35 -0700

    feat: added location screen

--- navigations/Tabs.tsx
+++ navigations/Tabs.tsx
@@ -9,7 +9,6 @@ import {
 import { Fontisto, SimpleLineIcons, Ionicons } from "@expo/vector-icons";
 import Location from "../screens/Location";
 import Cart from "../screens/Cart";
-import Profile from "../screens/Profile";
 import CartHeader from "../components/core/CartHeader";
 const Tab = createBottomTabNavigator();
 type iconTypes = {
@@ -55,6 +54,7 @@ const Tabs = () => {
         component={Location}
         name="Location"
         options={{
+          headerShown: false,
           tabBarIcon: ({ focused }: iconTypes): any => (
             <StyledView className="flex items-center space-y-1">
               <SimpleLineIcons
@@ -95,26 +95,6 @@ const Tabs = () => {
                 className={`${focused ? "text-pink" : "text-darkText"} `}
               >
                 My Cart
-              </StyledText>
-            </StyledView>
-          ),
-        }}
-      />
-      <Tab.Screen
-        component={Profile}
-        name="Profile"
-        options={{
-          tabBarIcon: ({ focused }: iconTypes): any => (
-            <StyledView className="flex items-center space-y-1">
-              <Ionicons
-                name="person-outline"
-                size={24}
-                color={focused ? "#F28482" : "black"}
-              />
-              <StyledText
-                className={`${focused ? "text-pink" : "text-darkText"} `}
-              >
-                Me
               </StyledText>
             </StyledView>
           ),
--- screens/Location.tsx
+++ screens/Location.tsx
@@ -1,14 +1,37 @@
 import React from "react";
+import { SafeAreaView } from "react-native";
+import MapView from "react-native-maps";
 import {
-  StyledText,
+  StyledTouchableOpacity,
   StyledView,
 } from "../components/styledComponents/StyledComponents";
+import { AntDesign } from "@expo/vector-icons";
 
-const Location = () => {
+const Location = ({ navigation }) => {
   return (
-    <StyledView>
-      <StyledText>Location</StyledText>
-    </StyledView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StyledView style={{ flex: 1 }}>
+        <StyledTouchableOpacity className="bg-white w-10 h-10 rounded-lg flex items-center justify-center absolute top-5 left-3 z-50">
+          <AntDesign
+            name="arrowleft"
+            size={24}
+            color="black"
+            onPress={() => navigation.navigate("Home")}
+          />
+        </StyledTouchableOpacity>
+        <MapView
+          style={{ flex: 1, width: "100%" }}
+          showsUserLocation={true}
+          followsUserLocation={true}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </StyledView>
+    </SafeAreaView>
   );
 };
 
